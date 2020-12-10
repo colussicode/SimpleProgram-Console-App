edb115fa1e6bff943d4dda67e9e7a5f1e7989ba2
Joao Colussi <colussicode@gmail.com>
2020-12-09 22:21:49 -0300

    Learning some concepts of C#

---
+++ MeuApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Product product = new Product(15, "Black Widow", 185.0, EProductType.Service);
+
+             Console.WriteLine(product.Id);
+             Console.WriteLine(product.Name);
+             Console.WriteLine(product.PriceInDolar(5.70f));
+             Console.WriteLine(product.Type);
+        }
+    }
+
+    struct Product
+    {
+        public Product(int id, string name, double price, EProductType type)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+        public int Id;
+        public string Name;
+        public double Price;
+        public EProductType Type;
+
+        public double PriceInDolar (double dolar)
+        {
+            return Price * dolar;
+        }
+    }   
+
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2
+    }
+}
+
