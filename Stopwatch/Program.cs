f5ee820b286873583ec82be83402669bbbbf7652
Joao Colussi <colussicode@gmail.com>
2020-12-09 22:49:34 -0300

    Done

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -31,9 +31,22 @@ namespace Stopwatch
             if(type ==0)
                 System.Environment.Exit(0);
 
-            Start(time * multiplier);
+            PreStart(time * multiplier);
         }
 
+        static void PreStart(int time)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
         static void Start(int time)
         {
             int currentTime = 0;
